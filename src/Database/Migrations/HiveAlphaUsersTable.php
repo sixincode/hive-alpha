53e7ef9f92958fb20561c3e09f39c8539b83870c
aalexkarim <contact.alexkarim@gmail.com>
2023-05-22 18:30:35 -0400

    auto-update-1684794633

--- src/Database/Migrations/HiveAlphaUsersTable.php
+++ src/Database/Migrations/HiveAlphaUsersTable.php
@@ -16,7 +16,7 @@ class HiveAlphaUsersTable
         $table->string('first_name')->after('id');
         $table->string('last_name')->after('first_name');
         $table->string('username')->after('last_name');
-        $table->string('phone')->after('email');
+        $table->string('phone')->after('email')->nullable();
         $table->tinyInteger('status')->default(0)->after('phone');
         $table->foreignId('current_tenant_id')->nullable();
         $table->string('provider_id')->unique()->nullable();
